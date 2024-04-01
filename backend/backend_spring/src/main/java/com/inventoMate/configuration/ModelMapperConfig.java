8f7ae031bb0af947f868c51e624dd1d1d533a4dd
SebaJuarez <sebajuarezz@gmail.com>
2024-03-31 23:52:14 -0300

    feat(backend) funcionalidad para gestionar los roles (no terminado)

---
+++ backend/backend_spring/src/main/java/com/inventoMate/configuration/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.inventoMate.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+	@Bean
+	ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
+}
