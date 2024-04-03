dd1f8119249d8d3982a9aa9e79e0f7ecb189c37a
SimonDiLeoGIT <saimondiley@gmail.com>
2024-04-03 12:47:52 -0300

    nothing

--- client/src/utils/Router.tsx
+++ client/src/utils/Router.tsx
@@ -1,17 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Login } from "../pages/Login"
 import { Home } from "../pages/Home"
-import { useAuth0 } from "@auth0/auth0-react";
 import { Navbar } from "../components/Navbar";
-// import { Navbar } from "../Navbar/Navbar"
 
 export const Router = () => {
-
-  const { isAuthenticated } = useAuth0();
-
-  if (!isAuthenticated) {
-    return <Login />
-  }
 
   return (
     <BrowserRouter>
@@ -22,7 +14,6 @@ export const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-      {/* <Footer /> */}
     </BrowserRouter>
   )
 }
