12ce88e802e006181536e75b76116951cbcdfdab
Fedesin <fedesimone31@gmail.com>
2024-04-19 23:12:50 -0300

    deje comentado el test para GetEmpresaByOwnerAuth0Id() pero no se porque falla

--- backend/backend_spring/src/test/java/com/inventoMate/testCreateEmpresa.java
+++ backend/backend_spring/src/test/java/com/inventoMate/testCreateEmpresa.java
@@ -1,6 +1,9 @@
 package com.inventoMate;
 
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,22 +12,41 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
 import com.auth0.exception.Auth0Exception;
 import com.inventoMate.*;
 import com.inventoMate.dtos.empresas.EmpresaDTO;
 import com.inventoMate.dtos.users.OwnerDTO;
 import com.inventoMate.dtos.users.UsuarioDTO;
+import com.inventoMate.entities.Empresa;
+import com.inventoMate.entities.Usuario;
+import com.inventoMate.repositories.EmpresaRepository;
 import com.inventoMate.repositories.UsuarioRepository;
 import com.inventoMate.services.EmpresaService;
 import com.inventoMate.services.UsuarioService;
+import com.inventoMate.services.impl.EmpresaServiceImpl;
 import com.inventoMate.services.impl.UsuarioServiceImpl;
+
+import static org.hamcrest.CoreMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 
 @ExtendWith(MockitoExtension.class)
 public class testCreateEmpresa {
 	
   	@Mock
     private EmpresaService empresaServiceMock;
+  	@Mock
+  	private UsuarioService usuarioServiceMock;
+  	@Mock
+  	private EmpresaRepository empresaRepositoryMock;
+  	@Mock
+    private UsuarioRepository usuarioRepositoryMock;
+    @Mock
+    private ModelMapper modelMapperMock;
+
+    @InjectMocks
+    private EmpresaServiceImpl empresaService;
 
     @Test
     public void testCreateEmpresa() throws Auth0Exception {
@@ -49,4 +71,44 @@ public class testCreateEmpresa {
         // Verificación
         Assertions.assertEquals(1L, empresaCreadaResultado.getIdEmpresa());
     }
+    
+   /*@Test
+    public void testGetEmpresaByOwnerAuht0Id() throws Auth0Exception {
+        // Datos simulados
+        String idAuth0 = "idAuth0";
+        Long userId = 1L;
+
+        // Simulación de UsuarioDTO
+        OwnerDTO ownerSimulado = new OwnerDTO();
+        ownerSimulado.setIdUsuario(userId);
+        ownerSimulado.setIdAuth0(idAuth0);
+
+        // Simulación de EmpresaDTO
+        EmpresaDTO empresaDTOsimulada = new EmpresaDTO();
+        empresaDTOsimulada.setIdEmpresa(1L);
+        empresaDTOsimulada.setOwner(ownerSimulado);
+
+        // Simulación de Usuario
+        Usuario usuarioSimulado = new Usuario();
+        usuarioSimulado.setIdUsuario(userId);
+        usuarioSimulado.setIdAuth0(idAuth0);
+
+        // Simulación de Empresa
+        Empresa empresaSimulada = new Empresa();
+        empresaSimulada.setIdEmpresa(1L);
+        empresaSimulada.setOwner(usuarioSimulado);
+
+        // Comportamiento esperado del mock de UsuarioRepository
+        when(usuarioRepositoryMock.findByIdAuth0(idAuth0)).thenReturn(Optional.of(usuarioSimulado));
+
+        // Comportamiento esperado del mock de EmpresaRepository
+        when(empresaRepositoryMock.findByOwner(Mockito.any(Usuario.class))).thenReturn(Optional.of(empresaSimulada));
+
+        // Ejecución del método bajo prueba
+        EmpresaDTO empresaObtenida = empresaService.getEmpresaByOwnerAuht0Id(idAuth0);
+
+        // Verificación
+        Assertions.assertNotNull(empresaObtenida);
+        Assertions.assertEquals(1L, empresaObtenida.getIdEmpresa());
+    }*/
 }
