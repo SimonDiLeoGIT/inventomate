4ce526f92c19d19d0e5ee8a7bc34cf8662b05b81
SimonDiLeoGIT <saimondiley@gmail.com>
2024-05-29 18:12:14 -0300

    feat(despliegue): Agrega carga de base de datos

---
+++ initmysql.sql
@@ -0,0 +1,5 @@
+INSERT INTO `bd-inventomate.roles` (nombre_rol, descripcion, id_rol_auth0) VALUES 
+('Executive Manager', 'Recibe toda la información + la toma de decisiones de los otros roles con respecto a esta información (Es el rol principal)', 'rol_Srg9Adz4UR3BDUz5'),
+('Procurement Manager', 'Recibe sugerencias de siguientes pedidos a realizar', 'rol_YQ7k5ic0TQhDbAZz'),
+('Sales Analyst', 'Recibe informes de ventas + sugerencias de qué hacer con los productos estancados', 'rol_H6p8RFAAe6X91IHu'),
+('Trend Analyst', 'Recibe información de nuevas tendencia', 'rol_PQoRMVTvHNj8ezmp');
