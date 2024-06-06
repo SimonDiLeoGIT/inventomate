17cb8dd764900ff5954e198b87d2092cbf7cc008
SimonDiLeoGIT <saimondiley@gmail.com>
2024-06-06 12:12:02 -0300

    fix(despliegue): update workflow

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,12 @@ import './styles/index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App'
 
-const domain = import.meta.env.VITE_APP_AUTH0_DOMAIN
-const clientId = import.meta.env.VITE_APP_AUTH0_CLIENT_ID
-const audience = import.meta.env.VITE_APP_AUTH0_AUDIENCE
+// const domain = import.meta.env.VITE_APP_AUTH0_DOMAIN
+// const clientId = import.meta.env.VITE_APP_AUTH0_CLIENT_ID
+// const audience = import.meta.env.VITE_APP_AUTH0_AUDIENCE
+const domain = process.env.VITE_APP_AUTH0_DOMAIN || ''
+const clientId = process.env.VITE_APP_AUTH0_CLIENT_ID || ''
+const audience = process.env.VITE_APP_AUTH0_AUDIENCE || ''
 console.log(domain)
 console.log(clientId)
 console.log(audience)
