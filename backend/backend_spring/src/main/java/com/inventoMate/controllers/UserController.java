185d236c4a8cdfa7a71449f37b42ff700c43c29a
Fedesin <fedesimone31@gmail.com>
2024-06-07 16:38:57 -0300

    prueba cors 2

--- backend/backend_spring/src/main/java/com/inventoMate/controllers/UserController.java
+++ backend/backend_spring/src/main/java/com/inventoMate/controllers/UserController.java
@@ -31,7 +31,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/users")
-@CrossOrigin(origins = "http://35.227.28.109/")
+@CrossOrigin(origins = "http://35.227.28.109")
 @Validated
 public class UserController {
 
