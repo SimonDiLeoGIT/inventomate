b9ef716f637503e1eee07c8923277e13d4c21e89
SimonDiLeoGIT <saimondiley@gmail.com>
2024-06-07 19:37:48 -0300

    feat(despliegue): crea el namespace

--- client/src/utils/Services/api.service.ts
+++ client/src/utils/Services/api.service.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosError } from 'axios';
 import { handleApiError } from '../errorHander';
 
-export const url = import.meta.env.VITE_APP_API_SERVER_URL || 'http://spring-inventomate:8080/'
+export const url = import.meta.env.VITE_APP_API_SERVER_URL || 'http://spring-inventomate.inventomate.svc.cluster.local:8080/'
 
 export class ApiService {
 
