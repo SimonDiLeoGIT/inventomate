263b798abae81ccf2cce685687ae8b88fc7fdf8f
SebaJuarez <113382513+SebaJuarez@users.noreply.github.com>
2024-06-08 21:48:03 -0300

    feat(backend): creo job para sincronizar roles de auth0 con bd local automaticamente

---
+++ backend/backend_spring/src/main/java/com/inventoMate/configuration/bd/DataBaseConfig.java
@@ -0,0 +1,41 @@
+package com.inventoMate.configuration.bd;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.auth0.exception.Auth0Exception;
+import com.inventoMate.entities.Rol;
+import com.inventoMate.repositories.RolRepository;
+import com.inventoMate.services.RoleAuth0Service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Configuration
+public class DataBaseConfig {
+
+	private final RoleAuth0Service auth0RoleService;
+	private final RolRepository rolRepository;
+	
+	@PostConstruct
+	public void initializeRoles() {
+		if (rolRepository.count() == 0) {
+			System.out.println("Sincronizando roles de Auth0 con BD local..");
+			try {
+				auth0RoleService.getAllRoles().forEach(role -> {
+					Rol rol = new Rol();
+					rol.setDescripcion(role.getDescription());
+					rol.setIdRolAuth0(role.getId());
+					rol.setNombreRol(role.getName());
+					rol.setUsuarios(Collections.emptyList());
+					rolRepository.save(rol);
+				});
+			} catch (Auth0Exception e) {
+				System.out.println("No se pudo sincronizar los roles de Auth0 con BD local..");
+				e.printStackTrace();
+			}
+		}
+	}
+}
