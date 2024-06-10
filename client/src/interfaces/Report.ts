ec80695cee0e884ee97ab830d0fc91153fe24223
SimonDiLeoGIT <saimondiley@gmail.com>
2024-06-10 16:28:17 -0300

    feat(front): comienza a implementar la paginación del back

--- client/src/interfaces/Report.ts
+++ client/src/interfaces/Report.ts
@@ -1,6 +1,37 @@
+// interface Report {
+//   content: Content[]
+//   pageable: Pageable
+//   last: boolean
+//   totalPages: number
+//   totalElements: number
+//   size: number
+//   sort: Sort[]
+//   first: boolean
+//   numberOfElements: number
+//   empty: boolean
+// }
+
 interface Report {
   id: number,
   fecha: string,
   tipoInforme: string,
   visto: boolean
 }
+
+// interface Pageable {
+//   pageNumber: number
+//   pageSize: number
+//   sort: Sort[]
+//   offset: number
+//   unpaged: boolean
+//   paged: boolean
+// }
+
+// interface Sort {
+//   direction: string
+//   property: string
+//   ignoreCase: boolean
+//   nullHanding: string
+//   ascending: boolean
+//   descending: boolean
+// }
