5f88b3bc0edc4ab678b6f76984e10c766becb553
Fedesin <fedesimone31@gmail.com>
2024-06-11 20:12:01 -0300

    (up) lo dejo como antes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,13 +4,6 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import fs from 'fs'
-import path from 'path'
-
-// Ruta a los archivos de certificado y clave privada
-const certPath = path.resolve(__dirname, 'cert.pem')
-const keyPath = path.resolve(__dirname, 'key.pem')
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,10 +19,7 @@ export default defineConfig({
     host: true,
     origin: "http://localhost:5173",
     // Habilita HTTPS en el servidor de desarrollo
-    https: {
-      key: fs.readFileSync(keyPath),
-      cert: fs.readFileSync(certPath),
-    },
+    https: true,
   },
   test: {
     globals: true,
