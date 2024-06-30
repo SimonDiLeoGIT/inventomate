0d6720ba94ad3e36c168ec68e220613e39527660
SimonDiLeoGIT <saimondiley@gmail.com>
2024-06-30 15:55:05 -0300

    (up)

--- backend/backend_spring/src/main/java/com/inventoMate/controllers/InvitacionController.java
+++ backend/backend_spring/src/main/java/com/inventoMate/controllers/InvitacionController.java
@@ -1,6 +1,5 @@
 package com.inventoMate.controllers;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,14 +23,11 @@ public class InvitacionController {
 	private final InvitacionService invitacionService;
 	private final SucursalService sucursalService;
 
-	@Value("${invitation.redirect.url}")
-	private String redirectUrl;
-
 	@GetMapping("/aceptar/{token}")
 	public ModelAndView aceptarInvitacion(@PathVariable String token) {
 		InvitacionSucursal invitacionSucursal = invitacionService.getInvitacionByToken(token);
 		sucursalService.addUserWithRoles(invitacionSucursal);
-		return new ModelAndView(new RedirectView(redirectUrl));
+		return new ModelAndView(new RedirectView("http://localhost:5173/"));
 	}
 
 	@GetMapping("/rechazar/{token}")
