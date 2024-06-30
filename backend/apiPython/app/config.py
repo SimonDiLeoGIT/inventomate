39bb0f3b0cd846767389ef1eafa01cc3d8f91dd0
SimonDiLeoGIT <saimondiley@gmail.com>
2024-06-30 11:27:18 -0300

    (up)

--- backend/apiPython/app/config.py
+++ backend/apiPython/app/config.py
@@ -1,2 +1,11 @@
 #MONGO_URI = "mongodb://localhost:27017/inventomate_db"
-MONGO_URI = "mongodb://mongodb:27017/inventomate_db"
+#MONGO_URI = "mongodb://mongodb:27017/inventomate_db"
+
+import os
+
+mongo_uri = os.getenv('MONGO_URI')
+
+if mongo_uri:
+    print(f"MONGO_URI: {mongo_uri}")
+else:
+    print("No se encontró la variable de entorno MONGO_URI")
