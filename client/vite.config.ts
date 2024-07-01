390351bfd9cbd337f8aa420338b7ee631b0284ec
SimonDiLeoGIT <saimondiley@gmail.com>
2024-07-01 16:27:06 -0300

    .

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,12 +14,12 @@ export default defineConfig({
     strictPort: true,
   },
   server: {
+    // https: false,
     port: 5173,
     strictPort: true,
     host: true,
     origin: "http://localhost:5173",
     // Habilita HTTPS en el servidor de desarrollo
-    https: true,
   },
   test: {
     globals: true,
